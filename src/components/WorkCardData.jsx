ded293b4eed35decc30a3916dd226b1ccf267972
Pushpraj1522002 <106546262+Pushpraj1522002@users.noreply.github.com>
2024-03-06 18:40:50 +0000

    change the content of text

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -9,7 +9,7 @@ const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Ecommerc Website Design",
-        text: "thriving facebook community and top ranked podcast, amy inspires a grounded, tangible and self-affirming sense of 'wow! I really can do this' for over 100,000 online enterpreneurs. Best selling marketing courses",
+        text: "In summary, for small changes or individual contributors, committing directly to the main branch can be efficient. However, for larger teams or more complex projects, creating a new branch and starting a pull request provides benefits such as collaboration, code review, and quality control. It's essential to choose the approach that best fits your project's needs and your team's workflow.",
         view: "https://www.google.com/search?q=google&rlz=1C1RXQR_enIN1096IN1096&oq=google&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTITCAEQLhiDARjHARixAxjRAxiABDIOCAIQRRgnGDsYgAQYigUyBggDEEUYQTIGCAQQRRhBMgYIBRBFGEEyBggGEAUYQDIGCAcQRRg80gEINjQ3NGoxajeoAgCwAgA&sourceid=chrome&ie=UTF-8",
     },
     {
