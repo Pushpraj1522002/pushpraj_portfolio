45b25b72afaac79681502cc758a88b3156754eab
Pushpraj1522002 <106546262+Pushpraj1522002@users.noreply.github.com>
2024-03-07 04:07:41 +0000

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import "./FooterStyle.css"
 
 import React from 'react'
-import {FaHome,FaInstagram,FaLinkedin,FaMailBulk,FaPhone, FaTwitter} from 'react-icons/fa'
+import {FaGithub, FaHome,FaInstagram,FaLinkedin,FaMailBulk,FaPhone, FaTwitter} from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -12,8 +12,8 @@ const Footer = () => {
                 <div className="location">
                     <FaHome size={20} style={{color: "white",marginRight:"2rem"}}/>
                     <div>
-                        <p>Nanadi.</p>
-                        <p>chikodi-591244.</p>
+                        <p>Nanadi</p>
+                        <p>chikodi-125944.</p>
                         <p>Belgavi, Karnataka, India.</p>
                     </div>
                 </div>
@@ -36,7 +36,7 @@ const Footer = () => {
                 <a href="https://www.instagram.com/pushpraj_desai/" target="_blank" rel="noopener noreferrer"> <FaInstagram size={30} style={{color: "white",marginRight:"1rem"}}/></a>
                 <a href="https://twitter.com/pushpraj1763" target="_blank" rel="noopener noreferrer"> <FaTwitter size={30} style={{color: "white",marginRight:"1rem"}}/></a>
                 <a href="https://www.linkedin.com/in/pushpraj-desai-869655240/" target="_blank" rel="noopener noreferrer"> <FaLinkedin size={30} style={{color: "white",marginRight:"1rem"}}/></a>   
-                <a href="https://github.com/Pushpraj1522002" target="_blank" rel="noopener noreferrer"> <FaGithub size={30} style={{color: "white",marginRight:"1rem"}}/></a>
+                <a href="https://github.com/Pushpraj1522002" target="_blank" rel="noopener noreferrer"> <FaGithub size={30} style={{color: "white",marginRight:"1rem"}}/></a>   
                 </div>
             </div>
         </div>
